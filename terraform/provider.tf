420215630a56b2633536641e89f3a19ccadda6b8
HermannDj <harnaud.djoko@gmail.com>
2025-11-15 14:05:42 -0500

    fix: fmt -check

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,13 +9,12 @@ terraform {
 
 provider "azurerm" {
   features {
-    
   }
   subscription_id = var.subscription_id
-  
+
   # Configuration pour l'authentification OIDC
-  use_oidc = true
+  use_oidc   = true
   oidc_token = var.oidc_token
-  tenant_id = var.tenant_id
-  client_id = var.client_id
+  tenant_id  = var.tenant_id
+  client_id  = var.client_id
 }
