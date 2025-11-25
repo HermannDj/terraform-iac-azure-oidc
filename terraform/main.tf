99413ddbd5d0cef4ad49dbd751bd3e9d046fa4e4
HermannDj <harnaud.djoko@gmail.com>
2025-11-25 17:52:34 -0500

    Use environments to create infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,27 @@
-resource "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "main" {
   name     = var.resource_group_name
-  location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   sku                 = "Basic"
   admin_enabled       = true
 }
 
 resource "azurerm_service_plan" "asp" {
   name                = var.app_service_plan_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
